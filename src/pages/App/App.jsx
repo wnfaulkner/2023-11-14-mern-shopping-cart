bd4be07faa28fc8d8ae0d6d70c6832038776d8ce
Jim Clark <jim.clark@generalassemb.ly>
2022-12-07 13:37:28 -0800

    sync-cafe-5-neworderpage

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -5,21 +5,17 @@ import { getUser } from '../../utilities/users-service';
 import AuthPage from '../AuthPage/AuthPage';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
-import NavBar from '../../components/NavBar/NavBar';
 
 export default function App() {
   const [user, setUser] = useState(getUser());
   return (
     <main className="App">
       { user ?
-        <>
-          <NavBar user={user} setUser={setUser} />
-          <Routes>
-            {/* client-side route that renders the component instance if the path matches the url in the address bar */}
-            <Route path="/orders/new" element={<NewOrderPage />} />
-            <Route path="/orders" element={<OrderHistoryPage />} />
-          </Routes>
-        </>
+        <Routes>
+          {/* client-side route that renders the component instance if the path matches the url in the address bar */}
+          <Route path="/orders/new" element={<NewOrderPage user={user} setUser={setUser} />} />
+          <Route path="/orders" element={<OrderHistoryPage />} />
+        </Routes>
         :
         <AuthPage setUser={setUser} />
       }
--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,8 +1,16 @@
 import { useState, useEffect, useRef } from 'react';
 import * as itemsAPI from '../../utilities/items-api';
+import './NewOrderPage.css';
+import { Link } from 'react-router-dom';
+import Logo from '../../components/Logo/Logo';
+import MenuList from '../../components/MenuList/MenuList';
+import CategoryList from '../../components/CategoryList/CategoryList';
+import OrderDetail from '../../components/OrderDetail/OrderDetail';
+import UserLogOut from '../../components/UserLogOut/UserLogOut';
 
-export default function NewOrderPage() {
+export default function NewOrderPage({ user, setUser }) {
   const [menuItems, setMenuItems] = useState([]);
+  const [activeCat, setActiveCat] = useState('');
   const categoriesRef = useRef([]);
 
   // The empty dependency array causes the effect
@@ -12,11 +20,27 @@ export default function NewOrderPage() {
       const items = await itemsAPI.getAll();
       categoriesRef.current = [...new Set(items.map(item => item.category.name))];
       setMenuItems(items);
+      setActiveCat(categoriesRef.current[0]);
     }
     getItems();
   }, []);
 
   return (
-    <h1>NewOrderPage</h1>
+    <main className="NewOrderPage">
+      <aside>
+        <Logo />
+        <CategoryList
+          categories={categoriesRef.current}
+          activeCat={activeCat}
+          setActiveCat={setActiveCat}
+        />
+        <Link to="/orders" className="button btn-sm">PREVIOUS ORDERS</Link>
+        <UserLogOut user={user} setUser={setUser} />
+      </aside>
+      <MenuList
+        menuItems={menuItems.filter(item => item.category.name === activeCat)}
+      />
+      <OrderDetail />
+    </main>
   );
 }
