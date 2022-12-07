13aa7e393c47021e55d5be6ba58a524defb15198
Jim Clark <jim.clark@generalassemb.ly>
2022-12-07 12:50:32 -0800

    sync-cafe-4-categories

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,14 +1,16 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import * as itemsAPI from '../../utilities/items-api';
 
 export default function NewOrderPage() {
   const [menuItems, setMenuItems] = useState([]);
+  const categoriesRef = useRef([]);
 
   // The empty dependency array causes the effect
   // to run ONLY after the FIRST render
   useEffect(function() {
     async function getItems() {
       const items = await itemsAPI.getAll();
+      categoriesRef.current = [...new Set(items.map(item => item.category.name))];
       setMenuItems(items);
     }
     getItems();
