498f2181009906d5473ed4f73a015c8021087c3b
Jim Clark <jim.clark@generalassemb.ly>
2022-12-07 12:29:42 -0800

    sync-cafe-3-fetch-items

--- server.js
+++ server.js
@@ -26,6 +26,10 @@ const port = process.env.PORT || 3001;
 // Put API routes here, before the "catch all" route
 app.use('/api/users', require('./routes/api/users'));
 
+// Protect all routes below from anonymous users
+const ensureLoggedIn = require('./config/ensureLoggedIn');
+app.use('/api/items', ensureLoggedIn, require('./routes/api/items'));
+
 // The following "catch all" route (note the *) is necessary
 // to return the index.html on all non-AJAX/API requests
 app.get('/*', function(req, res) {
--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,4 +1,19 @@
+import { useState, useEffect } from 'react';
+import * as itemsAPI from '../../utilities/items-api';
+
 export default function NewOrderPage() {
+  const [menuItems, setMenuItems] = useState([]);
+
+  // The empty dependency array causes the effect
+  // to run ONLY after the FIRST render
+  useEffect(function() {
+    async function getItems() {
+      const items = await itemsAPI.getAll();
+      setMenuItems(items);
+    }
+    getItems();
+  }, []);
+
   return (
     <h1>NewOrderPage</h1>
   );
