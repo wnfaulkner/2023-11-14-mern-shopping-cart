dff872d29492138cdb356dd35273b3a4061f6625
Jim Clark <jim.clark@generalassemb.ly>
2022-12-08 08:02:15 -0800

    sync-cafe-8-shop-1-virtuals

--- models/order.js
+++ models/order.js
@@ -5,6 +5,14 @@ const itemSchema = require('./itemSchema');
 const lineItemSchema = new Schema({
   qty: { type: Number, default: 1 },
   item: itemSchema
+}, {
+  timestamps: true,
+  toJSON: { virtuals: true }
+});
+
+lineItemSchema.virtual('extPrice').get(function() {
+  // 'this' keyword is bound to the lineItem document
+  return this.qty * this.item.price;
 });
 
 const orderSchema = new Schema({
@@ -17,6 +25,18 @@ const orderSchema = new Schema({
   isPaid: { type: Boolean, default: false } 
 }, {
   timestamps: true
+});
+
+orderSchema.virtual('orderTotal').get(function() {
+  return this.lineItems.reduce((total, item) => total + item.extPrice, 0);
+});
+
+orderSchema.virtual('orderQty').get(function() {
+  return this.lineItems.reduce((total, item) => total + item.qty, 0);
+});
+
+orderSchema.virtual('orderId').get(function() {
+  return this.id.slice(-6).toUpperCase();
 });
 
 module.exports = mongoose.model('Order', orderSchema);
