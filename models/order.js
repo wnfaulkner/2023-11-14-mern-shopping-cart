4feccfdb13409ea19375985ae9542c656fa8dcab
Jim Clark <jim.clark@generalassemb.ly>
2022-12-08 07:43:33 -0800

    sync-cafe-7-shop-1-models

---
+++ models/order.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const itemSchema = require('./itemSchema');
+
+const lineItemSchema = new Schema({
+  qty: { type: Number, default: 1 },
+  item: itemSchema
+});
+
+const orderSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  lineItems: [lineItemSchema],
+  isPaid: { type: Boolean, default: false } 
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Order', orderSchema);
