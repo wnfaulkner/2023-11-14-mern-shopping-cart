7961e2aa9b3f362b86663b3798ed8772974fc7fa
wnf <william@fluxrme.com>
2023-11-30 17:51:11 -0600

    resets code to sei code at end of pt 1, step 5

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -4,10 +4,13 @@ const ordersCtrl = require('../../controllers/api/orders');
 
 // GET /api/orders/cart
 router.get('/cart', ordersCtrl.cart);
+
 // POST /api/orders/cart/items/:id
 router.post('/cart/items/:id', ordersCtrl.addToCart);
+
 // POST /api/orders/cart/checkout
 router.post('/cart/checkout', ordersCtrl.checkout);
+
 // POST /api/orders/cart/qty
 router.put('/cart/qty', ordersCtrl.setItemQtyInCart);
 
